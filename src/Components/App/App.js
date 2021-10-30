2f1d144e5a3b7501f6d4614d40b60e9f8c5da27d
Aaron Huggins <huggins.aaron@outlook.com>
2021-10-30 09:36:11 -0700

    Pass down search result and render result

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,52 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'ATWA',
+          artist: 'System of a Down',
+          album: 'Toxicity',
+          id: 1
+        },
+        {
+          name: 'Toxicity',
+          artist: 'System of a Down',
+          album: 'Toxicity',
+          id: 2
+        },
+        {
+          name: 'Psycho',
+          artist: 'System of a Down',
+          album: 'Toxicity',
+          id: 3
+        }
+      ],
+      playlistName: 'Cool Tunes',
+      playlistTracks: [
+        {
+          name: 'First Kill',
+          artist: 'Amon Amarth',
+          album: 'Jomsviking',
+          id: 4
+        },
+        {
+          name: 'Buzzin',
+          artist: 'Alina Baraz',
+          album: 'Buzzin',
+          id: 5
+        },
+        {
+          name: 'Spirit Crusher',
+          artist: 'Death',
+          album: 'The Sound of Perseverance',
+          id: 6
+        }
+      ],
+    }
+  }
   render() {
     return (
       <div>
@@ -13,8 +59,11 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults = {this.state.searchResults}/>
+            <Playlist 
+              playlistName = {this.state.playlistName}
+              playlistTracks = {this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Track.css';
 class Track extends React.Component {
     renderAction() {
+        let isRemoval = true;
         if(isRemoval) {
             return <button className="Track-action">-</button>
         } else {
@@ -12,8 +13,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
               <div className="Track-information">
-                <h3><!-- track name will go here --></h3>
-                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                <h3>{this.props.track.name}</h3>
+                <p>{this.props.track.artist} | {this.props.track.album}</p>
               </div>
               {this.renderAction}
             </div>
